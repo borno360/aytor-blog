142a69ae10d9d7939ab5e7a1f8bc4136caebee47
borno360 <115914996+borno360@users.noreply.github.com>
2023-04-04 00:36:20 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,5 @@
+$(`.product_slick_slider`).slick({
+    slidesToShow:4,
+    prevArrow:`<i class=" product_slider_icon_left fa-solid fa-chevron-left">`,
+    nextArrow:`<i class=" product_slider_icon_right fa-solid fa-chevron-right">`,             
+ })
